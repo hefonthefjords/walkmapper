e9ecc9aeb2766f536d7aa85790abf6229cf24cee
hefonthefjords <hefman@gmail.com>
2025-06-04 15:13:36 +0100

    add functions to walk class

--- lib/classes/walk.dart
+++ lib/classes/walk.dart
@@ -1,26 +1,61 @@
 // data structure class that describes an instance of a recorded walk
 
+import 'dart:io';
+
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Walk {
 
   String walkTitle = "";
   List<LatLng> waypoints = [];
-  double travelDistance = 0.0;
-  double maxDistance = 0.0;
-  double elevationChange = 0.0;
-  var walkPhotos = [];
+  double totalTravelDistanceMetres = 0.0;
+  double totalTravelDistanceMiles = 0.0;
+  double maxDistanceFromStartMetres = 0.0;
+  double maxDistanceFromStartMiles = 0.0;
+  double totalElevationChangeMetres = 0.0;
+  List<File> walkPhotos = [];
 
   void changeWalkTitle (String title){
-  walkTitle = title;
+    walkTitle = title;
   }
 
   void addWaypoint (LatLng waypoint){
+
+    // add the new waypoint to the list
     waypoints.add(waypoint);
+
+    // if there's more than one waypoint in the list
+    if (waypoints.length > 1){
+      // recalculate distances and elevation here
+      for (int i = 0; i < waypoints.length - 1; i++){
+        // insert calcs here
+      }
+    }
+
   }
 
   List<LatLng> readWaypoints() {
     return waypoints;
+  }
+
+  double readTravelDistanceMetres(){
+    return totalTravelDistanceMetres;
+  }
+
+  double readTravelDistanceMiles(){
+    return totalTravelDistanceMiles;
+  }
+
+  double readMaxDistanceFromStartMetres(){
+    return maxDistanceFromStartMetres;
+  }
+
+  double readMaxDistanceFromStartMiles(){
+    return maxDistanceFromStartMiles;
+  }
+
+  List<File> readWalkPhotos(){
+    return walkPhotos;
   }
 }
 
