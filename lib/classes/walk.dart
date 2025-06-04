2d717282126c467ba733b646b5335ce954090a7f
hefonthefjords <hefman@gmail.com>
2025-06-04 19:43:17 +0100

    added calculation of total travel distance

--- lib/classes/walk.dart
+++ lib/classes/walk.dart
@@ -1,7 +1,7 @@
 // data structure class that describes an instance of a recorded walk
 
 import 'dart:io';
-
+import 'dart:math';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class Walk {
@@ -27,14 +27,10 @@ class Walk {
     // add the new waypoint to the list
     waypoints.add(waypoint);
 
-    // if there's more than one waypoint in the list
-    if (waypoints.length > 1){
-      // recalculate distances and elevation here
-      for (int i = 0; i < waypoints.length - 1; i++){
-        // insert calcs here
-      }
-    }
-
+    // recalculate distances etc
+    totalTravelDistanceMetres = calculateTotalDistanceMetres();
+    totalTravelDistanceMiles = totalTravelDistanceMetres / 0.000621371;
+    // need to add max distances and elevation calcs here!!
   }
 
   List<LatLng> readWaypoints() {
@@ -60,5 +56,36 @@ class Walk {
   List<File> readWalkPhotos(){
     return walkPhotos;
   }
+
+  double calculateTotalDistanceMetres() {
+    // if there is only one waypoint then no travel has happened
+    if (waypoints.length < 2) return 0.0;
+
+    // Earth's radius constant (metres)
+    const double R = 6371000; 
+    double totalDistance = 0.0;
+
+    for (int i = 0; i < waypoints.length - 1; i++) {
+      LatLng start = waypoints[i];
+      LatLng end = waypoints[i + 1];
+
+      double dLat = (end.latitude - start.latitude) * pi / 180;
+      double dLon = (end.longitude - start.longitude) * pi / 180;
+
+      double a = sin(dLat / 2) * sin(dLat / 2) +
+                cos(start.latitude * pi / 180) * cos(end.latitude * pi / 180) *
+                sin(dLon / 2) * sin(dLon / 2);
+      double c = 2 * atan2(sqrt(a), sqrt(1 - a));
+
+      // Distance in meters
+      totalDistance += R * c; 
+    }
+
+    return totalDistance; // Returns total distance in meters
+  }
+
+
+
+
 }
 
