45de5e84c6069a5a267071bb9a7d459fbd6c9c54
hefonthefjords <hefman@gmail.com>
2025-07-14 16:19:44 +0100

    included changes from code review - mostly removing redundant commentary and obsolete code

--- lib/pages/walkreviewpage.dart
+++ lib/pages/walkreviewpage.dart
@@ -102,54 +102,6 @@ class _GoogleMapsFlutterState extends State<WalkeReviewPage> {
     _mapController!.animateCamera(CameraUpdate.newLatLngBounds(bounds, 275));
   }
 
-  // Toggle tracking & manage walk sessions
-  // void _toggleTracking() {
-  //   setState(() {
-  //     // if not currently tracking
-  //     if (!_isTracking) {
-  //       // create a new walk object
-  //       widget.walk= Walk();
-  //       // assign the walk title to be the datetime string by default
-  //       widget.walk.changeWalkTitle(
-  //         widget.walk.walkStartTime.toIso8601String(),
-  //       );
-  //       // set the first waypoint as the current position manually so that waypoint[0] is always the starting location
-  //       widget.walk.addWaypoint(_currentPosition!);
-  //     } else {
-  //       // if we are currently tracking then a walk has been recorded
-  //       if (widget.walk!= null) {
-  //         // set final waypoint of walk
-  //         widget.walk.addWaypoint(_currentPosition!);
-
-  //         // store the current walk as a completed walk in the completed walks list
-  //         _completedWalks.add(widget.walk); // Save completed walk
-
-  //         // store completed walk in hive
-  //         boxWalk.put("key_${widget.walk.walkTitle}", widget.walk;
-
-  //         // set current walk to null
-  //         // widget.walk= null;
-
-  //         // wipe out polyline
-  //         _polylines = {};
-
-  //         // review the completed walks that have been stored DEBUG
-  //         // reviewCompletedWalks();
-  //       }
-  //     }
-  //     _isTracking = !_isTracking;
-  //   });
-  //}
-
-  // DEBUG: print completed walks data to console
-  // void reviewCompletedWalks() {
-  //   for (Walk walk in _completedWalks) {
-  //     print(
-  //       "Walk startetd at ${walk.walkStartTime.toIso8601String()} titled \"${walk.walkTitle} has ${walk.waypoints.length} waypoints recorded.",
-  //     );
-  //   }
-  // }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -198,71 +150,5 @@ class _GoogleMapsFlutterState extends State<WalkeReviewPage> {
                 ),
                ),
     );
-
-
-
-
-
-                    // widget.walk.totalTravelDistanceMetres == null ||
-                    //         !_isTracking
-                    //     ? Text("")
-                    //     : Column(
-                    //       mainAxisAlignment: MainAxisAlignment.center,
-                    //       children: [
-                    //         Text(
-                    //           "Current Walk Stats",
-                    //           style: TextStyle(fontWeight: FontWeight.bold),
-                    //         ),
-                    //         Text(
-                    //           "Total Distance Walked: ${widget.walk.totalTravelDistanceMetres} metres",
-                    //         ),
-                    //         Text(
-                    //           "# of Waypoints Recorded : ${widget.walk.waypoints.length}",
-                    //         ),
-                    //         Text("Current Address: $address"),
-
-                            // ElevatedButton.icon(
-                            //   icon: Icon(Icons.camera_alt),
-                            //   label: const Text(
-                            //     "Take A Photo",
-                            //     style: TextStyle(fontSize: 20),
-                            //   ),
-                            //   onPressed: () async {
-                            //     await takePhoto(ImageSource.camera);
-                            //   },
-                            // ),
-                        //   ],
-                        // ),
-                        
-                    // ElevatedButton.icon(
-                    //   onPressed: _toggleTracking,
-                    //   label: Text(
-                    //     _isTracking
-                    //         ? "End Recording your Walk"
-                    //         : "Begin Recording Your Walk",
-                    //     style:
-                    //         _isTracking
-                    //             ? TextStyle(
-                    //               color: Colors.redAccent,
-                    //               fontWeight: FontWeight.bold,
-                    //               fontSize: 20,
-                    //             )
-                    //             : TextStyle(
-                    //               fontWeight: FontWeight.bold,
-                    //               fontSize: 20,
-                    //             ),
-                    //   ),
-                    // ),
-    //               ],
-    //             ),
-    //           ),
-    // );
-  
-
-  // @override
-  // void dispose() {
-  //   _mapController?.dispose();
-  //   super.dispose();
-  // }
-}
+  }
 }
